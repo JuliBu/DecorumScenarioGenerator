f24579b9823146c2896160f239fe2c4b9fa6e76b
JulianBurghoff_WS <burghoff@uni-wuppertal.de>
2024-01-20 15:08:55 +0100

    Implemented position getter

--- common/rooms.py
+++ common/rooms.py
@@ -1,13 +1,50 @@
+from common.objects import DecorumObject
+
+
 class Room:
-    def __init__(self, name, wall_color):
+    def __init__(self, name: str, wall_color: str):
+        assert name in ["Bedroom1", "Bedroom2", "LivingRoom", "Kitchen"]
+        assert wall_color in ["red", "green", "yellow", "blue"]
         self.name = name
         self.wall_color = wall_color
-        self.objects = []
-        self.object_order = []
+        self.left_object = None
+        self.middle_object = None
+        self.right_object = None
         self.players = []
 
-    def add_object(self, decorum_object):
-        # Überprüfen, ob der Objekttyp bereits im Raum vorhanden ist
+    def get_position_of_object(self, d_object: DecorumObject) -> str:
+        d_obj_type = d_object.obj_type
+        if self.name == "Bedroom1":
+            if d_obj_type == "Curiosity":
+                return "left"
+            elif d_obj_type == "Painting":
+                return "middle"
+            elif d_obj_type == "Lamp":
+                return "right"
+        elif self.name == "Bedroom2":
+            if d_obj_type == "Painting":
+                return "left"
+            elif self.name == "Lamp":
+                return "middle"
+            elif self.name == "Curiosity":
+                return  "right"
+        elif self.name == "LivingRoom":
+            if d_obj_type == "Curiosity":
+                return "left"
+            elif d_obj_type == "Lamp":
+                return "middle"
+            elif d_obj_type == "Painting":
+                return "right"
+        elif self.name == "Kitchen":
+            if d_obj_type == "Lamp":
+                return "left"
+            elif d_obj_type == "Painting":
+                return "middle"
+            elif d_obj_type == "Curiosity":
+                return "right"
+        raise ValueError(f"Could not get position for {self.name=}, {d_obj_type=}.")
+
+    def add_object(self, decorum_object: DecorumObject):
         if decorum_object.obj_type in self.object_order:
             raise ValueError(f"{decorum_object.obj_type} ist bereits im Raum vorhanden")
 
