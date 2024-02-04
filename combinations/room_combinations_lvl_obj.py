c350a363045d6d26d0d3ecaec70e350f420b10a1
JulianBurghoff_WS <burghoff@uni-wuppertal.de>
2024-02-04 20:41:12 +0100

    Implemented roomcombinations with walls

--- combinations/room_combinations_lvl_obj.py
+++ combinations/room_combinations_lvl_obj.py
@@ -1,3 +1,4 @@
+import inspect
 import itertools
 import random
 
@@ -34,8 +35,9 @@ class RoomItemCombinations:
 
         check_for_empty_list(new_combs)
         self.object_combinations = new_combs
+        return f"{self.room_name=}, {nr_items=}, {color=}, {mode=}"
 
-    def filter_items_by_style_and_quantity(self, nr_items: int, style: str, mode: str):
+    def filter_items_by_style_and_quantity(self, nr_items: int, style: str, mode: str) -> str:
         assert 0 <= nr_items <= 3
         assert style in STYLES
         assert mode in ["min", "max"]
@@ -57,19 +59,21 @@ class RoomItemCombinations:
 
         check_for_empty_list(new_combs)
         self.object_combinations = new_combs
+        return f"{self.room_name=}, {nr_items=}, {style=}, {mode=}"
 
-    def filter_availability_of_type(self, type: str, should_be_available: bool):
-        assert type in OBJ_TYPES
+    def filter_availability_of_type(self, obj_type: str, should_be_available: bool) -> str:
+        assert obj_type in OBJ_TYPES
         new_combs = []
         for obj_comb in self.object_combinations:
             left_color, middle_color, right_color = obj_comb
             left_type, middle_type, right_type = [get_type_from_room_and_pos(self.room_name, pos) for pos in ["left", "middle", "right"]]
             for obj_type, obj_color in zip([left_type, middle_type, right_type],
                                            [left_color, middle_color, right_color]):
-                if obj_type == type and (obj_color is not None) == should_be_available:
+                if obj_type == obj_type and (obj_color is not None) == should_be_available:
                     new_combs.append(obj_comb)
         check_for_empty_list(new_combs)
         self.object_combinations = new_combs
+        return f"{self.room_name=}, {obj_type=}, {should_be_available=}"
 
     def get_random_method(self):
         params = {
@@ -80,18 +84,53 @@ class RoomItemCombinations:
             'obj_type': random.choice(OBJ_TYPES),
             'should_be_available': random.choice([True, False])
         }
+        methods = [
+            self.filter_items_by_color_and_quantity,
+            self.filter_items_by_style_and_quantity,
+            self.filter_availability_of_type
+        ]
+        random_method = random.choice(methods)
+        method_args = {param: params[param] for param in params if param in inspect.signature(random_method).parameters}
+        return random_method(**method_args)
+
 
     def __len__(self):
         return len(self.object_combinations)
 
     def __str__(self):
-        return self.object_combinations
+        out_str = ""
+        for obj_comb in self.object_combinations:
+            for obj_list in obj_comb:
+                out_str = out_str + str(obj_list) + ", "
+            out_str += "\n"
+        return out_str
 
 
 class RoomCombinationsWithWalls:
-    def __init__(self, item_combinations):
+    def __init__(self, room_name: str, item_combinations):
         wall_colors = OBJ_COLORS
+        self.room_name = room_name
         self.room_wall_combinations = list(itertools.product(item_combinations, wall_colors))
+
+    def __len__(self):
+        return len(self.room_wall_combinations)
+
+    def filter_color_and_quantity_wall(self, nr_items: int, mode: str):
+        assert 0 <= nr_items <= 3
+        assert mode in ["min", "max"]
+
+        new_combs = []
+        for obj_comb, wall_color in self.room_wall_combinations:
+            if mode == "min" and obj_comb.count(wall_color) >= nr_items:
+                new_combs.append((obj_comb, wall_color))
+            elif mode == "max" and obj_comb.count(wall_color) <= nr_items:
+                new_combs.append((obj_comb, wall_color))
+
+        check_for_empty_list(new_combs)
+        self.room_wall_combinations = new_combs
+        return f"{self.room_name=}, {nr_items=}, {mode=}"
+
+    # ToDo: ggf Funktion wie Wenn antikes Objekt in diesem Raum enthalten, darf die Wandfarbe nicht blau sein
 
 
 class UpperFloorCombinationsOnlyRooms:
--- commands/run.py
+++ commands/run.py
@@ -1,4 +1,6 @@
-from combinations.room_combinations_lvl_obj import RoomItemCombinations
+import random
+
+from combinations.room_combinations_lvl_obj import RoomItemCombinations, RoomCombinationsWithWalls
 from house.rooms.rooms import Room
 
 init_bedroom1 = Room("bedroom1", "red")
@@ -9,12 +11,38 @@ init_kitchen = Room("kitchen", "blue")
 
 def iter_modifications():
     bedroom1_combs = RoomItemCombinations('bedroom1')
-    bedroom1_combs.filter_items_by_color_and_quantity(2, "blue", "max")
-    bedroom1_combs.filter_items_by_color_and_quantity(1, "blue", "min")
-    bedroom1_combs.filter_items_by_color_and_quantity(1, "red", "min")
-    bedroom1_combs.filter_availability_of_type("lamp", False)
-    print(len(bedroom1_combs))
-    print(bedroom1_combs)
+    bedroom2_combs = RoomItemCombinations('bedroom2')
+    livingroom_combs = RoomItemCombinations('livingroom')
+    kitchen_combs = RoomItemCombinations('kitchen')
+    rooms_combs = [bedroom1_combs, bedroom2_combs, livingroom_combs, kitchen_combs]
+    # bedroom1_combs.filter_items_by_color_and_quantity(2, "blue", "max")
+    # bedroom1_combs.filter_items_by_color_and_quantity(1, "blue", "min")
+    # bedroom1_combs.filter_items_by_color_and_quantity(1, "red", "min")
+    # bedroom1_combs.filter_availability_of_type("lamp", False)
+
+    while (len(bedroom1_combs) * len(bedroom2_combs) * len(livingroom_combs) * len(kitchen_combs) > 10000000):
+        current_room = random.choice(rooms_combs)
+        print(current_room.get_random_method())
+        print(len(current_room))
+        print(len(bedroom1_combs) * len(bedroom2_combs) * len(livingroom_combs) * len(kitchen_combs))
+
+    bedroom1_wall_combs = RoomCombinationsWithWalls(bedroom1_combs.room_name, bedroom1_combs.object_combinations)
+    bedroom2_wall_combs = RoomCombinationsWithWalls(bedroom2_combs.room_name, bedroom2_combs.object_combinations)
+    livingroom_wall_combs = RoomCombinationsWithWalls(livingroom_combs.room_name, livingroom_combs.object_combinations)
+    kitchen_wall_combs = RoomCombinationsWithWalls(kitchen_combs.room_name, kitchen_combs.object_combinations)
+
+    bedroom1_wall_combs.filter_color_and_quantity_wall(2, "min")
+
+
+    #ToDo: Debug -> why 3 list entries?
+    """
+    nr_items=2, color='blue', mode='max'
+obj_type='lamp', should_be_available=True
+nr_items=3, color='red', mode='min'
+nr_items=3, color='green', mode='max'
+"""
+
+    # print(bedroom1_combs)
 
 
 if __name__ == '__main__':
