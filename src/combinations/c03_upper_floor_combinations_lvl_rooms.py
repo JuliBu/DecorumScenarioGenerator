056cf6459bcb80858b37f1ee4a71ede69b8b9513
JulianBurghoff_WS <burghoff@uni-wuppertal.de>
2024-03-26 22:46:06 +0100

    Implemented filter_floor_nr_items

--- src/combinations/c03_upper_floor_combinations_lvl_rooms.py
+++ src/combinations/c03_upper_floor_combinations_lvl_rooms.py
@@ -48,6 +48,37 @@ class UpperFloorCombinationsOnlyRooms:
 
         return ConditionOutput(eng_output, ger_output)
 
+    def filter_floor_nr_items(self, nr_items_on_floor: int, mode: str) -> ConditionOutput:
+        assert 0 <= nr_items_on_floor <= 6
+        assert mode in ["min", "max", "equals"]
+
+        new_combs = []
+        for upper_floor in self.upper_floor_combinations_only_rooms:
+            left_room, right_room = upper_floor
+            obj_counter = 6 - (left_room[0] + right_room[0]).count(None)
+            if mode == "min" and obj_counter >= nr_items_on_floor:
+                new_combs.append(upper_floor)
+            elif mode == "max" and obj_counter <= nr_items_on_floor:
+                new_combs.append(upper_floor)
+            elif mode == "equals" and obj_counter == nr_items_on_floor:
+                new_combs.append(upper_floor)
+        check_for_inval_cond(new_combs, len(self.upper_floor_combinations_only_rooms))
+        self.upper_floor_combinations_only_rooms = new_combs
+
+        if mode == "min":
+            ger_output = f"In der oberen Etage müssen mindestens {nr_items_on_floor} Objekte zu finden sein!"
+            eng_output = f"In the upper floor, there must be at least {nr_items_on_floor} objects!"
+        elif mode == "max":
+            ger_output = f"In der oberen Etage dürfen maximal {nr_items_on_floor} Objekte zu finden sein!"
+            eng_output = f"In the upper floor, there may be a maximum of {nr_items_on_floor} objects!"
+        elif mode == "equals":
+            ger_output = f"In der oberen Etage müssen exakt {nr_items_on_floor} Objekte zu finden sein!"
+            eng_output = f"In the upper floor, there have to be exact {nr_items_on_floor} objects!"
+        else:
+            raise ValueError
+
+        return ConditionOutput(eng_output, ger_output)
+
 
     def new_generic_function(self):
         # Asserts
@@ -89,6 +120,7 @@ class UpperFloorCombinationsOnlyRooms:
         }
         methods_with_weights = [
             MethodWithWeight(self.filter_floor_items_by_color_and_quantity, 5),
+            MethodWithWeight(self.filter_floor_nr_items, 3),
         ]
         random_method = get_weighted_random_method(methods_with_weights)
         method_args = {param: params[param] for param in params if param in inspect.signature(random_method).parameters}
