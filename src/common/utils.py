14f2b8c4df795284d632bc8ada2fc519df19f9a7
JuliBu <84986368+JuliBu@users.noreply.github.com>
2024-03-29 15:03:16 +0100

    Asserting that new_combs not longer than old_combs

--- src/common/utils.py
+++ src/common/utils.py
@@ -4,6 +4,8 @@ from typing import List
 
 def check_for_inval_cond(new_combs: List, old_len: int):
     new_len = len(new_combs)
+    if new_len > old_len:
+        raise AssertionError
     if new_len < 1 or new_len == old_len:
         raise ValueError("Invalid condition: E.g. list would be empty or condition has no effect!")
 
